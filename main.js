cd9565c16d554892e78d520c43530442f0c0bd87
sherikbro <sherzodomonov8776@gmail.com>
2024-05-24 10:53:10 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const sd=document.querySelector(".navbar");
+const bar=document.querySelector(".burger");
+bar.addEventListener('click', ()=>{
+    sd.classList.toggle('active');
+});
+
+const save =()=>{
+    sd.classList.toggle('active');
+}
